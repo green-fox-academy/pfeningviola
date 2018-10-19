a039808338e348f897ed93701e2b675a6affe135
Viola Pfening <pfeningviola@gmail.com>
2018-10-19 09:41:33 +0200

    Exercises 26 and 27 completed

--- week-03/day-2/Expression_control_flow/src/Loops.java
+++ week-03/day-2/Expression_control_flow/src/Loops.java
@@ -17,8 +17,36 @@ public class Loops {
         System.out.println("Please enter a number: ");
         int x = scannerInt.nextInt();
 
-        for(int y = 1; y <= 10; y++){
-            System.out.println(y + " * " + x + " = "+ (y*x));
+        for(int y = 0; y < 10; y++){
+            System.out.println((y + 1) + " * " + x + " = "+ ((y+1)*x));
+        }
+
+        //CountFromTo
+        System.out.println("Please enter a number: ");
+        int g1 = scannerInt.nextInt();
+        System.out.println("Please enter one more number: ");
+        int g2 = scannerInt.nextInt();
+
+        if (g1 >= g2){
+            System.out.println("The second number should be bigger");
+        }else{
+            for(int h=0; h < (g2-g1); h++){
+                System.out.println(h+g1);
+            }
+        }
+
+        //FizzBuzz
+        for(int j=0; j < 100; j++){
+            if(((j+1) % 3 == 0) && ((j+1) % 5 == 0)){
+                System.out.println("FizzBuzz");
+            }else if((j+1) % 3 == 0){
+                System.out.println("Fizz");
+            }else if((j+1) % 5 == 0) {
+                System.out.println("Buzz");
+            }else{
+                System.out.println(j+1);
+            }
+
         }
 
 
