66f76b78dd4e93bc5707790143abe45ac1ce3e5a
Viola Pfening <pfeningviola@gmail.com>
2018-11-07 10:07:55 +0100

    ConnectTheDots and StarryNight completed

--- week-06/day-01/DrawingAndGraphics/src/ConnectTheDots.java
+++ week-06/day-01/DrawingAndGraphics/src/ConnectTheDots.java
@@ -2,25 +2,21 @@ import java.awt.*;
 import javax.swing.*;
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
-public class FunctionToCenter {
+public class ConnectTheDots {
     public static void mainDraw(Graphics graphics) {
-        int xCoord = 0;
-        int yCoord = 0;
-        for (int i = 0; i < (WIDTH / 20); i++) {
-            lineDrawing(graphics, xCoord, 0);
-            lineDrawing(graphics, 0, yCoord);
-            lineDrawing(graphics, xCoord, 320);
-            lineDrawing(graphics, 320, yCoord);
+        int[][] example1 = {{10, 10}, {290,  10}, {290, 290}, {10, 290}};
+        int[][] example2 = {{50, 100}, {70, 70}, {80, 90}, {90, 90}, {100, 70}, {120, 100}, {85, 130}, {50, 100}};
+        connectDots(graphics, example1);
+        connectDots(graphics, example2);
 
-            xCoord += 20;
-            yCoord += 20;
-        }
-        lineDrawing(graphics, 320, 320);
     }
 
-    public static void lineDrawing (Graphics g, int x, int y) {
-        g.setColor(Color.RED);
-        g.drawLine(x, y, 160, 160);
+    public static void connectDots (Graphics g, int[][] coordinates) {
+        g.setColor(Color.GREEN);
+        for (int i = 0; i < coordinates.length - 1; i++) {
+            g.drawLine(coordinates[i][0], coordinates[i][1], coordinates[i + 1][0], coordinates[i + 1][1]);
+        }
+        g.drawLine(coordinates[coordinates.length - 1][0], coordinates[coordinates.length - 1][1], coordinates[0][0], coordinates[0][1]);
     }
 
     static int WIDTH = 320;
--- week-06/day-01/DrawingAndGraphics/src/StarryNight.java
+++ week-06/day-01/DrawingAndGraphics/src/StarryNight.java
@@ -2,25 +2,19 @@ import java.awt.*;
 import javax.swing.*;
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
-public class FunctionToCenter {
+public class StarryNight {
     public static void mainDraw(Graphics graphics) {
-        int xCoord = 0;
-        int yCoord = 0;
-        for (int i = 0; i < (WIDTH / 20); i++) {
-            lineDrawing(graphics, xCoord, 0);
-            lineDrawing(graphics, 0, yCoord);
-            lineDrawing(graphics, xCoord, 320);
-            lineDrawing(graphics, 320, yCoord);
+        int starsNumber = (int)(100 + (Math.random() * 500));
 
-            xCoord += 20;
-            yCoord += 20;
+        for (int i = 0; i < starsNumber; i++) {
+            int starsSize = (int)(1 + (Math.random() * 3));
+            int xCoord = (int)(Math.random() * (WIDTH - starsSize));
+            int yCoord = (int)(Math.random() * (HEIGHT - starsSize));
+            int color = (int)(30 + (Math.random() * 210));
+
+            graphics.setColor(new Color(color, color, color));
+            graphics.fillRect(xCoord, yCoord, starsSize, starsSize);
         }
-        lineDrawing(graphics, 320, 320);
-    }
-
-    public static void lineDrawing (Graphics g, int x, int y) {
-        g.setColor(Color.RED);
-        g.drawLine(x, y, 160, 160);
     }
 
     static int WIDTH = 320;
@@ -41,6 +35,7 @@ public class FunctionToCenter {
         @Override
         protected void paintComponent(Graphics graphics) {
             super.paintComponent(graphics);
+            this.setBackground(Color.BLACK);
             mainDraw(graphics);
         }
     }
