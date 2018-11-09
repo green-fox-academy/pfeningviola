3440dd2b245971e028806979c03f36daec8df5d2
Viola Pfening <pfeningviola@gmail.com>
2018-11-09 09:56:26 +0100

    EnvelopeStar without code duplication

--- week-06/day-4/DrawingProject/src/EnvelopeStar.java
+++ week-06/day-4/DrawingProject/src/EnvelopeStar.java
@@ -8,12 +8,9 @@ public class EnvelopeStar {
         int startPointUp = (WIDTH / 2) - (14 * lineDistance);
         int startPointDown = (WIDTH / 2) + (14 * lineDistance);
         int startPointLeft = (HEIGHT / 2) - (14 * lineDistance);
-        int startPointRight = (HEIGHT / 2) + (14 * lineDistance);
-
+        
         drawingEnvelopeStar1(graphics, 160, startPointUp, lineDistance);
-        drawingEnvelopeStar2(graphics, 160, startPointDown, lineDistance);
-        drawingEnvelopeStar3(graphics, startPointLeft, 160, lineDistance);
-        drawingEnvelopeStar4(graphics, startPointRight, 160, lineDistance);
+        drawingEnvelopeStar2(graphics, startPointLeft, 160, lineDistance);
 
         graphics.setColor(Color.GREEN);
         graphics.drawLine(160, startPointUp, 160, startPointDown);
@@ -21,41 +18,37 @@ public class EnvelopeStar {
 
     public static void drawingEnvelopeStar1(Graphics g, int topX, int topY, int lineDist) {
         g.setColor(Color.GREEN);
-        for (int i = 0; i < 14; i++) {
-            g.drawLine(topX, topY, 160 - lineDist, 160);
-            g.drawLine(topX, topY, 160 + lineDist, 160);
-            lineDist += 10;
-            topY += 10;
+        for (int h = 0; h < 2; h++) {
+            for (int i = 0; i < 14; i++) {
+                g.drawLine(topX, topY, 160 - lineDist, 160);
+                g.drawLine(topX, topY, 160 + lineDist, 160);
+                lineDist += 10;
+                if (h == 1) {
+                    topY -= 10;
+                } else {
+                    topY += 10;
+                }
+            }
+            lineDist = 10;
+            topY = (WIDTH / 2) + (14 * lineDist);
         }
     }
 
     public static void drawingEnvelopeStar2(Graphics g, int topX, int topY, int lineDist) {
         g.setColor(Color.GREEN);
-        for (int i = 0; i < 14; i++) {
-            g.drawLine(topX, topY, 160 - lineDist, 160);
-            g.drawLine(topX, topY, 160 + lineDist, 160);
-            lineDist += 10;
-            topY -= 10;
-        }
-    }
-
-    public static void drawingEnvelopeStar3(Graphics g, int topX, int topY, int lineDist) {
-        g.setColor(Color.GREEN);
-        for (int i = 0; i < 14; i++) {
-            g.drawLine(topX, topY, 160, 160 - lineDist);
-            g.drawLine(topX, topY, 160, 160 + lineDist);
-            lineDist += 10;
-            topX += 10;
-        }
-    }
-
-    public static void drawingEnvelopeStar4(Graphics g, int topX, int topY, int lineDist) {
-        g.setColor(Color.GREEN);
-        for (int i = 0; i < 14; i++) {
-            g.drawLine(topX, topY, 160, 160 - lineDist);
-            g.drawLine(topX, topY, 160, 160 + lineDist);
-            lineDist += 10;
-            topX -= 10;
+        for (int h = 0; h < 2; h++) {
+            for (int i = 0; i < 14; i++) {
+                g.drawLine(topX, topY, 160, 160 - lineDist);
+                g.drawLine(topX, topY, 160, 160 + lineDist);
+                lineDist += 10;
+                if (h == 1) {
+                    topX -= 10;
+                } else {
+                    topX += 10;
+                }
+            }
+            lineDist = 10;
+            topX = (HEIGHT / 2) + (14 * lineDist);
         }
     }
 
