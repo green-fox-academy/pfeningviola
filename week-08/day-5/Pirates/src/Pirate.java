13872f6a742bae17799e116208239c8b5206d96f
Viola Pfening <pfeningviola@gmail.com>
2018-11-23 13:33:37 +0100

    Pirate exercise: Pirate class created

---
+++ week-08/day-5/Pirates/src/Pirate.java
@@ -0,0 +1,108 @@
+public class Pirate {
+  private int rumLevel;
+  private boolean captain;
+  private boolean alive;
+  private boolean awake;
+
+  public Pirate(Boolean captain) {
+     this.rumLevel = 0;
+     this.captain = captain;
+     this.alive = true;
+     this.awake = true;
+  }
+
+  public Pirate(){
+    this.rumLevel = 0;
+    this.captain = false;
+    this.alive = true;
+    this.awake = true;
+  }
+
+  public void drinkSomeRum() {
+    if (checkAliveAndAwake()) {
+      this.rumLevel++;
+    }
+  }
+
+  public void howsItGoingMate() {
+    if (checkAliveAndAwake()) {
+      if (rumLevel < 5) {
+        System.out.println("Pour me anudder!");
+      } else {
+        System.out.println("Arghh, I'ma Pirate. How d'ya d'ink its goin?");
+        this.awake = false;
+      }
+    }
+  }
+   public void die() {
+    this.alive = false;
+    this.awake = false;
+   }
+
+   public void brawl (Pirate otherPirate) {
+    int whoWin = (int)(Math.random() * 3);
+    if (this.checkAliveAndAwake() && otherPirate.checkAliveAndAwake()) {
+      if (whoWin == 0){
+        this.alive = false;
+        this.awake = false;
+      } else if (whoWin == 1) {
+        otherPirate.alive = false;
+        otherPirate.awake = false;
+      } else {
+        this.awake = false;
+        otherPirate.awake = false;
+      }
+    }
+   }
+
+   public boolean checkAliveAndAwake() {
+     if (!this.alive) {
+       System.out.println("He's died.");
+     } else if (!this.awake) {
+       System.out.println("He's passed out!");
+     }
+     return (this.awake && this.alive);
+   }
+
+  public int getRumLevel() {
+    return rumLevel;
+  }
+
+  public void setRumLevel(int rumLevel) {
+    this.rumLevel = rumLevel;
+  }
+
+  public boolean isCaptain() {
+    return captain;
+  }
+
+  public void setCaptain(boolean captain) {
+    this.captain = captain;
+  }
+
+  public boolean isAlive() {
+    return alive;
+  }
+
+  public void setAlive(boolean alive) {
+    this.alive = alive;
+  }
+
+  public boolean isAwake() {
+    return awake;
+  }
+
+  public void setAwake(boolean awake) {
+    this.awake = awake;
+  }
+
+  @Override
+  public String toString() {
+    return "Pirate{" +
+        "rumLevel=" + rumLevel +
+        ", captain=" + captain +
+        ", alive=" + alive +
+        ", awake=" + awake +
+        '}';
+  }
+}
---
+++ week-08/day-5/Pirates/src/PirateMain.java
@@ -0,0 +1,29 @@
+public class PirateMain {
+  public static void main(String[] args) {
+    Pirate captain = new Pirate(true);
+    Pirate pirate1 = new Pirate();
+    Pirate pirate2 = new Pirate();
+
+    System.out.println(captain);
+
+    for (int i = 0; i < 5; i++) {
+      pirate1.drinkSomeRum();
+    }
+    System.out.println(pirate1);
+    pirate1.howsItGoingMate();
+    System.out.println(pirate1);
+    pirate1.drinkSomeRum();
+
+    System.out.println("---------------------");
+    captain.brawl(pirate2);
+    System.out.println(pirate2);
+    System.out.println(captain);
+    pirate2.howsItGoingMate();
+    captain.howsItGoingMate();
+    System.out.println("------------------------");
+
+    pirate1.die();
+    pirate1.drinkSomeRum();
+
+  }
+}
