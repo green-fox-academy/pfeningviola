af22e82543dc761074067bf4b9d3ebee28c4fe76
Viola Pfening <pfeningviola@gmail.com>
2018-12-10 10:40:31 +0100

    feat(Comparator): implemented ComparableTo for the Domino class

---
+++ week-11/day-1/Comparator/src/Domino.java
@@ -0,0 +1,46 @@
+public class Domino implements Comparable<Domino> {
+  private final int left;
+  private final int right;
+
+  public Domino(int left, int right) {
+    this.left = left;
+    this.right = right;
+  }
+
+  public int getLeftSide() {
+    return left;
+  }
+
+  public int getRightSide() {
+    return right;
+  }
+
+  @Override
+  public String toString() {
+    return "[" + left + ", " + right + "]";
+  }
+
+  @Override
+  public int compareTo(Domino other) {
+    int result = Integer.compare(this.left, other.left);
+    if (result == 0) {
+      result = Integer.compare(this.right, other.right);
+    }
+    return result;
+    
+    //Solution 2.
+    //if (this.left < other.left) {
+    //  return -1;
+    //} else if (this.left == other.left) {
+    //  if (this.right < other.right) {
+    //    return -1;
+    //  } else if (this.right == other.right) {
+    //    return 0;
+    //  } else {
+    //    return 1;
+    //  }
+    //} else {
+    //  return 1;
+    //}
+  }
+}
