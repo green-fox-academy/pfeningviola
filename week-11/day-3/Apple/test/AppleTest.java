53f3bf2d111b81d368ad1d53a77726caabcc2c1b
Viola Pfening <pfeningviola@gmail.com>
2018-12-12 22:16:27 +0100

    feat(Apple): task completed

---
+++ week-11/day-3/Apple/test/AppleTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AppleTest {
+
+  @Test
+  public void getApple() {
+    Apple apple = new Apple();
+    String expectedResult = "apple";
+
+    String result = apple.getApple();
+
+    assertEquals(expectedResult, result);
+  }
+}
