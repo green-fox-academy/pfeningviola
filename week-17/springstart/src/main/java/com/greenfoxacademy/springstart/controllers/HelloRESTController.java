6f4e464ee9c7543d002b16845847eb7991cead2b
Viola Pfening <pfeningviola@gmail.com>
2019-02-05 10:13:46 +0100

    feat(HelloRESTController): add greetings counter

--- week-17/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-17/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -5,12 +5,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
 public class HelloRESTController {
+  private AtomicLong numberOfCalls = new AtomicLong(0);
 
   @RequestMapping(value = "/greeting")
   public Greeting greeting(@RequestParam String name ) {
-    Greeting greeting = new Greeting(1, "Hello " + name + "!");
+    Greeting greeting = new Greeting(numberOfCalls.incrementAndGet(), "Hello " + name + "!");
     return greeting;
   }
 }
