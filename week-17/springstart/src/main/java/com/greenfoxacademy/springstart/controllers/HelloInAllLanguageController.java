b341a935a5b71ae215a09ab35a75c3844e767e0d
Viola Pfening <pfeningviola@gmail.com>
2019-02-06 23:34:09 +0100

    feat(HelloInAllLanguageController): add all greetings in random order, with random style

--- week-17/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloInAllLanguageController.java
+++ week-17/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloInAllLanguageController.java
@@ -1,24 +1,43 @@
 package com.greenfoxacademy.springstart.controllers;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller
-public class HelloDifferentLanguagesController {
+import java.util.ArrayList;
 
-  @RequestMapping("/greeting/languages")
+@Controller
+public class HelloInAllLanguageController {
+  @RequestMapping("/greeting/alllanguages")
   public String greeting(Model model, @RequestParam String name){
     String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
         "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
         "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
         "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
-    String randomGreetingInOtherLanguage = hellos[(int)(Math.random() * hellos.length)];
+    ArrayList<String> randomGreetings = orderGreetingsRandomly(hellos);
+    String textToDisplay = "";
 
-    model.addAttribute("name", "<span style ='" + styleRandom() + "'>" + name + "!</span>");
-    model.addAttribute("greetingInOtherLanguage", "<span style ='" + styleRandom() + "'>" + randomGreetingInOtherLanguage + ", </span>");
-    return "greetingLanguages";
+    for (String greeting : randomGreetings) {
+      textToDisplay = textToDisplay + "<span style ='" + styleRandom() + "'>" + greeting + ", " + name + "! </span>";
+    }
+    model.addAttribute("greetingsToDisplay", textToDisplay);
+    return "greetingLanguages2";
+  }
+
+  public ArrayList<String> orderGreetingsRandomly(String[] greetings){
+    ArrayList<String> originalGreetings = new ArrayList<>();
+    ArrayList<String> randomGreetings = new ArrayList<>();
+    for (String greeting : greetings) {
+      originalGreetings.add(greeting);
+    }
+
+    while (originalGreetings.size() != 0) {
+      int index = (int)(Math.random() * originalGreetings.size());
+      randomGreetings.add(originalGreetings.get(index));
+      originalGreetings.remove(index);
+    }
+
+    return randomGreetings;
   }
 
   public String styleRandom() {
